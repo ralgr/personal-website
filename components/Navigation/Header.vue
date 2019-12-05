60942fb99b229b204f37a1d6fc398297a10e1e34
ralgr <kimtorres40@yahoo.com>
2019-12-05 21:22:30 +0000

    Organized CSS code

--- components/Navigation/Header.vue
+++ components/Navigation/Header.vue
@@ -62,51 +62,92 @@
 </script>
 
 <style lang="css" scoped>
-        .container-fluid {
-            width: 100%;
-            margin-right: auto;
-            margin-left: auto;
-        }
+    /* Header normal settings: >1024 */
+    .header {
+        padding: 2.2rem 2.5rem 2.2rem 2.5rem;
+        -webkit-transition: all .3s ease;
+        transition: all .3s ease;
+        z-index: 6;
+    }
 
+    /* Header normal settings: 1024+ */
+    @media screen and (min-width: 1024px) {
         .header {
             padding: 4.2rem 3.5rem 4.2rem 3.5rem;
-            -webkit-transition: padding .3s ease;
-            transition: padding .3s ease;
-            -webkit-transition: background-color .3s ease;
-            transition: background-color .3s ease;
+            -webkit-transition: all .3s ease;
+            transition: all .3s ease;
             z-index: 6;
         }
 
+        /* Header scrolled settings */
         .header--fixed {
             background: white;
             padding: 1.2rem 3.5rem 1.2rem 3.5rem;
         }
+    }
 
-        .header__inner {
-            display: -webkit-box;
-            display: -ms-flexbox;
-            display: flex;
-            -webkit-box-sizing: inherit;
-                    box-sizing: inherit;
-            -webkit-box-pack: justify;
-                -ms-flex-pack: justify;
-                    justify-content: space-between;
-            -webkit-box-align: center;
+    /* Header inner div settings: Universal */
+    .header__inner {
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: flex;
+        -webkit-box-sizing: inherit;
+                box-sizing: inherit;
+        -webkit-box-pack: justify;
+            -ms-flex-pack: justify;
+                justify-content: space-between;
+        -webkit-box-align: center;
                 -ms-flex-align: center;
-                    align-items: center;
-        }
+                align-items: center;
+    }
 
-        .header__nav {
-            -webkit-box-sizing: inherit;
-                    box-sizing: inherit;
-            display: -webkit-box;
-            display: -ms-flexbox;
-            display: flex;
-            -webkit-box-align: center;
-                -ms-flex-align: center;
-                    align-items: center;
-        }
+    /* Header nav settings: Universal */
+    .header__nav {
+        -webkit-box-sizing: inherit;
+                box-sizing: inherit;
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: flex;
+        -webkit-box-align: center;
+            -ms-flex-align: center;
+                align-items: center;
+    }
 
+    /* Header logo container div settings: Universal */
+    .header__logo-container {
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: flex;
+        -webkit-box-pack: center;
+            -ms-flex-pack: center;
+                justify-content: center;
+        -ms-flex-line-pack: center;
+            align-content: center;
+    }
+
+    /* Header logo container div settings: >1024 */
+    .header__logo-icon {
+        width: 4rem;
+        height: 4rem;
+    }
+
+    /* Header logo container div settings: 1024+ */
+    @media screen and (min-width: 1024px) {
+       .header__logo-icon {
+            width: 5rem;
+            height: 5rem;
+        } 
+    }
+
+    /* Header large device nav links settings: >1024 */
+    /* ul CSS */
+    .header__ulist {
+        display: none;
+    }
+
+    /* Header large device nav links settings: 1024+ */
+    @media screen and (min-width: 1024px) {
+        /* ul CSS */
         .header__ulist {
             -webkit-box-sizing: inherit;
                     box-sizing: inherit;
@@ -116,6 +157,7 @@
             display: inline-block;
         }
 
+        /* li CSS */
         .header__list-item {
             -webkit-box-sizing: inherit;
                     box-sizing: inherit;
@@ -125,15 +167,18 @@
             display: inline-flex;   
         }
 
+        /* a CSS */
         .header__nav-link {
             font-size: 1.2rem;
             letter-spacing: .25px;
         }
 
+        /* Hover anim CSS: set width to 100% from 0% */
         .header__nav-link:hover::after {
             width: 100%;
         }
 
+        /* Hover anim CSS using ::after content */
         .header__nav-link::after {
             background-color: #000;
             content: "";
@@ -144,100 +189,90 @@
             transition: width .3s;
             width: 0;
         }
-
-        .header__logo-container {
-            display: -webkit-box;
-            display: -ms-flexbox;
-            display: flex;
-            -webkit-box-pack: center;
-                -ms-flex-pack: center;
-                    justify-content: center;
-            -ms-flex-line-pack: center;
-                align-content: center;
-        }
-
-        .header__logo-icon {
-            width: 77px;
-            height: 77px;
-        }
-
-    @media screen and (max-width: 1023px) {
-        .header__ulist {
-            display: none;
-        }
-
-        .header__burger {
-            display: inline-block;
-            cursor: pointer;
-            z-index: 13;
-        }
-    
-        .header__burger-bar1,
-        .header__burger-bar3 {
-            border-radius: 3px;
-            width: 35px;
-            height: 5px;
-            background-color: #333;
-            margin: 6px 0;
-            -webkit-transition: transform 0.2s;
-            transition: transform 0.2s;    
-        }
-    
-        .header__burger-bar2 {
-            border-radius: 3px;
-            width: 30px;
-            height: 5px;
-            background-color: #333;
-            margin: 6px 0;
-            -webkit-transition: width 0.2s;
-            transition: width 0.2s;     
-        }
-    
-        /* Rotate first bar */
-        .header__burger-transform .header__burger-bar1 {
-            -webkit-transform: rotate(-45deg) translate(-8.5px, 7px) ;
-            transform: rotate(-45deg) translate(-8.5px, 7px) ;
-        }
-    
-        /* Fade out the second bar */
-        .header__burger-transform .header__burger-bar2 {
-            width: 0;
-        }
-    
-        /* Rotate last bar */
-        .header__burger-transform .header__burger-bar3 {
-            -webkit-transform: rotate(45deg) translate(-8.5px, 7px) ;
-            transform: rotate(45deg) translate(-8.5px, -7px) ;
-        }
-
-        .header__burger-menu {
-            position: fixed;
-            top: 0;
-            right: 0;
-            border-radius: 0 0 0 100rem;
-            width: 0;
-            height: 0;
-            background: orange;
-            z-index: 12;
-            transition: all 0.3s ease;
-        }
-
-        .header__burger-on {
-            display: block;
-            width: 50vw;
-            height: 80vh;
-        }
-
-        .header__burger-items {
-            -webkit-box-sizing: inherit;
-                box-sizing: inherit;
-            list-style: none;
-            display: inline-block;
-        }
-
-        .header__burger-list-link {
-
-        }
     }
 
+    /* Header burger menu settings: >1024 */
+    .header__burger {
+        display: inline-block;
+        cursor: pointer;
+        z-index: 13;
+    }
+
+    /* Header burger menu settings: 1024+ */
+    @media screen and (min-width: 1024px) {
+       .header__burger {
+            display: none;
+        }
+    }
+    
+    .header__burger-bar1,
+    .header__burger-bar3 {
+        border-radius: 3px;
+        width: 35px;
+        height: 5px;
+        background-color: #333;
+        margin: 6px 0;
+        -webkit-transition: transform 0.2s;
+        transition: transform 0.2s;    
+    }
+    
+    .header__burger-bar2 {
+        border-radius: 3px;
+        width: 30px;
+        height: 5px;
+        background-color: #333;
+        margin: 6px 0;
+        -webkit-transition: width 0.2s;
+        transition: width 0.2s;     
+    }
+    
+    /* Rotate first bar */
+    .header__burger-transform .header__burger-bar1 {
+        -webkit-transform: rotate(-45deg) translate(-8.5px, 7px) ;
+        transform: rotate(-45deg) translate(-8.5px, 7px) ;
+    }
+    
+    /* Fade out the second bar */
+    .header__burger-transform .header__burger-bar2 {
+        width: 0;
+    }
+    
+    /* Rotate last bar */
+    .header__burger-transform .header__burger-bar3 {
+        -webkit-transform: rotate(45deg) translate(-8.5px, 7px) ;
+        transform: rotate(45deg) translate(-8.5px, -7px) ;
+    }
+
+    .header__burger-menu {
+        position: fixed;
+        top: 0;
+        right: 0;
+        border-radius: 0 0 0 100rem;
+        width: 0;
+        height: 0;
+        background: orange;
+        z-index: 12;
+        transition: all 0.3s ease;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        visibility: hidden;
+    }
+
+    .header__burger-on {
+        visibility: visible;
+        width: 50vw;
+        height: 80vh;
+    }
+
+    .header__burger-items {
+        -webkit-box-sizing: inherit;
+            box-sizing: inherit;
+        list-style: none;
+        display: inline-block;
+    }
+
+    .header__burger-list-link {
+
+    }
 </style>
